3e8a6a0428589aeb096a9ef8fa624d812c8e5e42
Myheartdies <76390200+Reacon01@users.noreply.github.com>
2023-12-01 15:25:22 -0600

    Add failure code to message model

--- src/ServiceMessageModel.java
+++ src/ServiceMessageModel.java
@@ -1,8 +1,10 @@
 public class ServiceMessageModel {
     public static int SERVICE_PUBLISH_REQUEST = 101;
     public static int SERVICE_UNPUBLISH_REQUEST = 102;
-    public static int SERVICE_DISCOVER_REQUEST = 201;
     public static int SERVICE_PUBLISH_OK = 103;
+    public static int SERVICE_PUBLISH_FAILED = 104;
+
+    public static int SERVICE_DISCOVER_REQUEST = 201;
     public static int SERVICE_DISCOVER_NOT_FOUND = 202;
     public static int SERVICE_DISCOVER_OK = 203;
 
