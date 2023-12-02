076a9615832a9e919ec8ad00fea7eeef6d307dcf
Myheartdies <76390200+Reacon01@users.noreply.github.com>
2023-12-02 16:25:17 -0600

    Add new message model TupleArgs This is for sending two arguments to a service

---
+++ src/TupleArgs.java
@@ -0,0 +1,9 @@
+
+/**
+ * A Message model for messages that contains two arguments.
+ * Used in WishListService's add and delete
+ */
+public class TupleArgs {
+    String arg0;
+    String arg1;
+}
